0551746d3d6a24bf8a963a2cd31887087a851efa
Dario Miñones <dariominones@cactus.ws>
2017-05-04 21:57:30 -0300

    .

--- config/settings-dev.js
+++ config/settings-dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port : process.env.NODE_PORT || 3000,
+  port : process.env.PORT || 3000,
   mongodb : {
     url : 'localhost',
     name: 'myproject',
--- config/settings-stage.js
+++ config/settings-stage.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port : process.env.NODE_PORT || 80,
+  port : process.env.PORT || 3000,
   mongodb : {
     url : 'ds119598.mlab.com',
     name: 'mongoose',
