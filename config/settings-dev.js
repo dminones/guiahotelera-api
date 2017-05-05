386cef2957f0a7d312713720701dde7701069c7f
Dario Miñones <dariominones@cactus.ws>
2017-05-04 22:28:12 -0300

    Environment extended settings

--- config/settings-dev.js
+++ config/settings-dev.js
@@ -1,8 +1,3 @@
 module.exports = {
-  port : process.env.PORT || 3000,
-  mongodb : {
-    url : 'localhost',
-    name: 'myproject',
-    port: '27017'
-  }
+  
 };
--- config/settings.js
+++ config/settings.js
@@ -1,3 +1,12 @@
+_ = require('underscore');
 var environment = process.env.ENVIRONMENT_NAME || 'dev';
-
-module.exports = require('./settings-'+ environment + '.js');
+var envSettings = require('./settings-'+ environment);
+var settings = {
+	  port : process.env.PORT || 3000,
+	  mongodb : {
+	    url : 'localhost',
+	    name: 'hotelguia',
+	    port: '27017'
+	  }
+}
+module.exports = _.extend(settings, envSettings);
