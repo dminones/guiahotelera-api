4c3b6eed039140e7196647bb9b61532accec79de
Dario Miñones <dariominones@cactus.ws>
2017-05-04 22:54:11 -0300

    Environment secret

---
+++ models/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var fs       = require("fs");
+var path     = require("path");
+var mongoose = require('mongoose');
+var settings = require('../config/settings');
+
+var authMongooseString =  (settings.mongodb.username && settings.mongodb.password) ? 
+              settings.mongodb.username+':'+settings.mongodb.password+'@' : 
+              '';
+var connectionString = 'mongodb://'+authMongooseString+settings.mongodb.url+':'+settings.mongodb.port+'/'+settings.mongodb.name;
+console.log(connectionString);
+mongoose.connect(connectionString);
+
+var db        = {};
+
+fs
+  .readdirSync(__dirname)
+  .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== "index.js");
+  })
+  .forEach(function(file) {
+    var model = require(path.join(__dirname, file));
+    db[model.modelName] = model;
+  });
+
+db.mongoose = mongoose;
+
+module.exports = db;
