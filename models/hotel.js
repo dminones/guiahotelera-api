a36049b1ef2007fa065b4b10cbff0cb7211b82de
Dario Miñones <dariominones@cactus.ws>
2017-05-04 23:31:05 -0300

    Hotel definition

--- models/hotel.js
+++ models/hotel.js
@@ -4,8 +4,11 @@ var mongoose = require('mongoose');
 // Define our beer schema
 var HotelSchema   = new mongoose.Schema({
   name: String,
-  rate: String,
-  newValue: String
+  type: String,
+  address: String,
+  phone: String,
+  email: String,
+  web: String
 });
 
 // Export the Mongoose model
